12af8a1ba9aa510d7be7ddb8b1ab472ad58d176e
saif88888 <saif_saeed_cs@yahoo.com>
2020-05-20 15:07:40 +0100

    Updates

---
+++ precache-manifest.80aeb89d13aa7b76e767443d55927f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1398cbbdcb424ab5dee487d09464002a",
+    "url": "/http//saif88888.github.io/react1/index.html"
+  },
+  {
+    "revision": "970039504bf6af207526",
+    "url": "/http//saif88888.github.io/react1/static/css/main.65e06bc7.chunk.css"
+  },
+  {
+    "revision": "2f7ccd1cecf8a857b264",
+    "url": "/http//saif88888.github.io/react1/static/js/2.18f50578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//saif88888.github.io/react1/static/js/2.18f50578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970039504bf6af207526",
+    "url": "/http//saif88888.github.io/react1/static/js/main.aae144de.chunk.js"
+  },
+  {
+    "revision": "aa790ab1a3fff08e385e",
+    "url": "/http//saif88888.github.io/react1/static/js/runtime-main.710f7d8e.js"
+  }
+]);
